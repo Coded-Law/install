ced010fdf1a9127bd3bf412e27bf710e580b0f93
Leon Debnath <10490509+S010MON@users.noreply.github.com>
2023-11-27 12:50:15 +0000

    Create jetbrains.sh

---
+++ jetbrains.sh
@@ -0,0 +1,12 @@
+# Install Jetbrains Toolbox
+cd ~/Downloads
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.1.1.18388.tar.gz
+sudo tar -xvzf ~/Downloads/jetbrains-toolbox-1.26.1.13138.tar.gz
+
+sudo mv jetbrains-toolbox-2.1.1.18388 /opt/
+
+cd /opt/jetbrains-toolbox-2.1.1.18388/
+
+rm -rf jetbrains-toolbox-2.1.1.18388.tar.gz
+
+./jetbrains-toolbox
