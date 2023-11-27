0f33d738b1512848674ee03dc5f20380952f0e0f
Leon Debnath <10490509+S010MON@users.noreply.github.com>
2023-11-27 12:50:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,42 +2,4 @@
 
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install python3 -y
-
-# Install 1password
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
- curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
- sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
- curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
- sudo apt update && sudo apt install 1password -y
-
-# Install Docker 
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-# Install Jetbrains Toolbox
-cd ~/Downloads
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.1.1.18388.tar.gz
-sudo tar -xvzf ~/Downloads/jetbrains-toolbox-1.26.1.13138.tar.gz
-
-sudo mv jetbrains-toolbox-2.1.1.18388 /opt/
-
-cd /opt/jetbrains-toolbox-2.1.1.18388/
-
-rm -rf jetbrains-toolbox-2.1.1.18388.tar.gz
-
-./jetbrains-toolbox
+sudo apt install vim -y
