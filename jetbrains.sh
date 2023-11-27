c9bd8555333ccb6bc737f03b94651759a1333ca3
Leon Debnath <10490509+S010MON@users.noreply.github.com>
2023-11-27 14:47:09 +0000

    Update jetbrains.sh

--- jetbrains.sh
+++ jetbrains.sh
@@ -1,9 +1,8 @@
 # Install Jetbrains Toolbox
-cd ~/Downloads
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.1.1.18388.tar.gz
-sudo tar -xvzf ~/Downloads/jetbrains-toolbox-1.26.1.13138.tar.gz
+wget -P ~/Downloads https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.1.1.18388.tar.gz
+sudo tar -xvzf ~/Downloads/jetbrains-toolbox-2.1.1.18388.tar.gz
 
-sudo mv jetbrains-toolbox-2.1.1.18388 /opt/
+sudo mv ~/Downloads/jetbrains-toolbox-2.1.1.18388 /opt/
 
 cd /opt/jetbrains-toolbox-2.1.1.18388/
 
